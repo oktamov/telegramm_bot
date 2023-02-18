f2cb2abc4c0f7c8f04a40f7e677266b9c9e3938a
oktamov <122670933+oktamov@users.noreply.github.com>
2023-02-18 15:37:27 +0500

    image_text

---
+++ image_to_text/main.py
@@ -0,0 +1,32 @@
+import telebot
+import os
+from PIL import Image
+import pytesseract
+
+bot = telebot.TeleBot('6182467188:AAFQPq1L1PCZ57NPb-q9OOcDlrToG7-xmrU')
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'Hello! Please send me a photo.')
+
+
+@bot.message_handler(content_types=['photo'])
+def photo(message):
+    file_id = message.photo[-1].file_id
+    file_info = bot.get_file(file_id)
+    downloaded_file = bot.download_file(file_info.file_path)
+    with open(f'{file_id}.jpg', 'wb') as f:
+        f.write(downloaded_file)
+    img = Image.open(f"{file_id}.jpg")
+    pytesseract.pytesseract.tesseract_cmd = r'Tesseract-OCR\tesseract'
+    text = pytesseract.image_to_string(img)
+    bot.send_message(message.chat.id, text)
+    if os.path.exists(f'{file_id}.jpg'):
+        os.remove(f'{file_id}.jpg')
+
+
+
+if __name__ == "__main__":
+    print("started")
+    bot.infinity_polling()
